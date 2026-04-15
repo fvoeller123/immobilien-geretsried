78bafb6b47ed6cd1d06fe067364dd3e45a8d8a79
fvoeller123 <FVoeller@web.de>
2026-04-15 10:45:45 +0200

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Immobilien Geretsried',
+        short_name: 'Immo GR',
+        description: 'Immobiliensuche 82538 Geretsried',
+        theme_color: '#0e0e0e',
+        background_color: '#0e0e0e',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
